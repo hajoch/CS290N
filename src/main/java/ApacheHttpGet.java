4c5b7bea256d14f87be32a757727ab58fce8bc29
Hajoch <hallvard.jore.christensen@gmail.com>
2015-03-10 21:20:41 -0700

    nothing

--- src/main/java/ApacheHttpGet.java
+++ src/main/java/ApacheHttpGet.java
@@ -1,21 +1,3 @@
-import com.jcabi.http.Request;
-import com.jcabi.http.Response;
-import com.jcabi.http.request.ApacheRequest;
-import com.jcabi.http.request.JdkRequest;
-import com.jcabi.http.response.JsonResponse;
-import org.apache.http.HttpHeaders;
-import com.jcabi.http.Request;
-import com.jcabi.http.request.ApacheRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 
 /**
  * Created by Kyrre on 09.03.2015.
