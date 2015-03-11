038eb6942c661c2d28a7ee5057c5961de1a506b2
kyrrelm <kyrre.l.moe@gmail.com>
2015-03-10 20:40:06 -0700

    derp

--- src/main/java/ApacheHttpGet.java
+++ src/main/java/ApacheHttpGet.java
@@ -23,39 +23,54 @@ import java.net.HttpURLConnection;
 public class ApacheHttpGet {
 
     public static void main(String[] args) {
-        try {
 
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpGet getRequest = new HttpGet(
-                    "http://localhost:9200/bank/_search?*");
-            getRequest.addHeader("accept", "application/json");
+        httpGet();
+        httpGet();
+        httpGet();
+        httpGet();
+        httpGet();
+        httpGet();
+    }
 
-            HttpResponse response = httpClient.execute(getRequest);
+    private static void httpGet() {
+        Runnable t = new Runnable() {
+            @Override
+            public void run() {
+                try {
 
-            if (response.getStatusLine().getStatusCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + response.getStatusLine().getStatusCode());
+                    DefaultHttpClient httpClient = new DefaultHttpClient();
+                    HttpGet getRequest = new HttpGet(
+                            "http://localhost:9200/bank/_search?*");
+                    getRequest.addHeader("accept", "application/json");
+
+                    HttpResponse response = httpClient.execute(getRequest);
+
+                    if (response.getStatusLine().getStatusCode() != 200) {
+                        throw new RuntimeException("Failed : HTTP error code : "
+                                + response.getStatusLine().getStatusCode());
+                    }
+
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader((response.getEntity().getContent())));
+
+                    String output;
+                    System.out.println("Output from Server .... \n");
+                    while ((output = br.readLine()) != null) {
+                        System.out.println(output);
+                    }
+
+                    httpClient.getConnectionManager().shutdown();
+
+                } catch (ClientProtocolException e) {
+
+                    e.printStackTrace();
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                }
             }
-
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader((response.getEntity().getContent())));
-
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
-
-            httpClient.getConnectionManager().shutdown();
-
-        } catch (ClientProtocolException e) {
-
-            e.printStackTrace();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-        }
-
+        };
+        t.run();
     }
 }
